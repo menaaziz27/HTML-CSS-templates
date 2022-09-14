95f699320767dab3bb8d5b691a62bfa2962223bf
Mena Aziz <menaaziz27@gmail.com>
2022-09-14 19:29:11 +0200

    deleted projects

--- MTMS/src/middlewares/asyncHandler.ts
+++
@@ -1,6 +0,0 @@
-export const asyncHandler = (fn: any) =>
-	function asyncUtilWrap(...args: any[]) {
-		const fnReturn = fn(...args);
-		const next = args[args.length - 1];
-		return Promise.resolve(fnReturn).catch(next);
-	};
--- appgain.io/url-shortener/db.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-
-CONNECTION_STRING = "mongodb+srv://root:admin@mycluster.dlfkm.mongodb.net/url-shortener?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('url-shortener')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
